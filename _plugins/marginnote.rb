db903a796b72997d197190ebc9f38a3facda54df
Su Hang <7192233+su1hang2@users.noreply.github.com>
2021-01-22 03:30:12 -0500

    fix side/marginnote numbering

--- _plugins/marginnote.rb
+++ _plugins/marginnote.rb
@@ -5,24 +5,29 @@
 module Jekyll
   class MarginnoteTagBlock < Liquid::Block
 
+    @@n = Hash.new { |h, k| h[k] = [] }
+
     def initialize(tag_name, args, tokens)
       super
-
       @marginnote_id = args.strip
-      if @marginnote_id == ""
-        @marginnote_id = ("marginnote-" + @n.to_s)
-      end
-
-      @n = 0
     end
 
     def render(context)
+      page_id = context.registers[:page].id
+      if @@n.key?(context.registers[:page].id)
+        @@n[page_id] += 1
+      else
+        @@n[page_id] = 0
+      end
+      if @marginnote_id == ""
+        @marginnote_id = "marginnote-#{@@n[page_id].to_s}"
+      end
+
       text = super
       rendered = ""
-      rendered << "<label for='toggle-marginnote-#{@n}' class='marginnote-symbol'>✥</label>"
-      rendered << "<input id='toggle-marginnote-#{@n}' class='marginnote-toggle' type='checkbox'>"
+      rendered << "<label for='toggle-marginnote-#{@@n[page_id]}' class='marginnote-symbol'>✥</label>"
+      rendered << "<input id='toggle-marginnote-#{@@n[page_id]}' class='marginnote-toggle' type='checkbox'>"
       rendered << "<span id='#{@marginnote_id}' class='marginnote'>#{text}</span>"
-      @n += 1
       rendered
     end
 
--- _plugins/sidenote.rb
+++ _plugins/sidenote.rb
@@ -5,26 +5,28 @@
 module Jekyll
   class SidenoteTagBlock < Liquid::Block
 
-    @@n = 0
+    @@n = Hash.new { |h, k| h[k] = [] }
 
     def initialize(tag_name, args, tokens)
       super
-
       @sidenote_id = args.strip
-      if @sidenote_id == ""
-        @sidenote_id = ("sidenote-" + @n.to_s)
-      end
-
-      @n = @@n
-      @@n += 1
-
     end
 
     def render(context)
+      page_id = context.registers[:page].id
+      if @@n.key?(context.registers[:page].id)
+        @@n[page_id] += 1
+      else
+        @@n[page_id] = 0
+      end
+      if @sidenote_id == ""
+        @sidenote_id = "sidenote-#{@@n[page_id].to_s}"
+      end
+
       text = super
       rendered = ""
-      rendered << "<sup><label for='toggle-sidenote-#{@n}' class='sidenote-number'></label></sup>"
-      rendered << "<input id='toggle-sidenote-#{@n}' class='sidenote-toggle' type='checkbox'>"
+      rendered << "<sup><label for='toggle-sidenote-#{@@n[page_id]}' class='sidenote-number'></label></sup>"
+      rendered << "<input id='toggle-sidenote-#{@@n[page_id]}' class='sidenote-toggle' type='checkbox'>"
       rendered << "<span id='#{@sidenote_id}' class='sidenote'>#{text}</span>"
       rendered
     end
