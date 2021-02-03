0e1c7f4412997da0c40fdcc7a3ee2d9e9b30781b
Su Hang <7192233+su1hang2@users.noreply.github.com>
2021-02-03 02:05:00 -0500

    use stacks collection instead of details pages

---
+++ _nonessential_plugins/stacks.rb
@@ -0,0 +1,39 @@
+# generate stacks documents for bib entries that don't appear as files
+
+module Jekyll
+  class StacksGenerator < Jekyll::Generator
+    include Scholar::Utilities
+
+    safe true
+    priority :highest
+
+    attr_reader :config
+
+    def generate(site)
+      @site = site
+      @config = Scholar.defaults.merge(site.config['scholar'] || {})
+
+      existing_entries = site.collections["stacks"].docs.map {
+        |e| e.id.split("/")[-1]
+      }
+
+      path_fmt = File.join(
+        File.dirname(site.collections["stacks"].docs[0].path),
+        "%{key}.md"
+      )
+      
+      for entry in entries
+        if existing_entries.include? entry.key
+          next
+        end
+        path = path_fmt % { :key => entry.key }
+        stack = Document.new(
+          path, :site => site, :collection => site.collections["stacks"]
+        )
+        stack.content = ""
+        site.collections["stacks"].docs << stack
+      end
+
+    end
+  end
+end
